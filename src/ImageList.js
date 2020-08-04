eabf8809fbcb07a865944772fc877e27532dcdbf
Michelle Stermitz <stermitzm@gmail.com>
2020-08-04 14:20:19 -0700

    Added file structure

---
+++ src/ImageList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './data.js'
+
+export default class ImageList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export class ImageItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
