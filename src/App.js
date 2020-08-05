bdf9dce2ab22a01226c4f1f092391d7d628643ad
Michelle Stermitz <stermitzm@gmail.com>
2020-08-04 17:20:45 -0700

    Filter is working

--- src/App.js
+++ src/App.js
@@ -4,15 +4,49 @@ import images from './data.js';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
 
+// const options = [
+//   'narwhal',
+//   'rhino',
+//   'unicorn',
+//   'unilego',
+//   'triceratops',
+//   'markhor',
+//   'mouflon',
+//   'addax',
+//   'chameleon',
+//   'lizard',
+//   'dragon'
+// ]
 
 
 class App extends React.Component {
+  state = {
+    filter: ''
+  }
+
+  handleKeyword = (e) => {
+    const keyword = e.target.value;
+
+    this.setState({ filter: keyword })
+  }
+
   render() {
+    const filteredImages = images.filter(image => image.keyword === this.state.filter)
+
   return (
       <div className="App">
         <Header/>
-        <ImageList images={images}/>
 
+        <select onChange={this.handleKeyword} name="images">
+                {
+                  images.map(image => <option key={image.url} value={image.keyword}>{image.title}</option>)
+                }
+            </select>
+          {
+            filteredImages.map(image => <p>{image.title}</p>)
+          }
+
+        <ImageList images={filteredImages}/>
       </div>
     );
   }
--- src/ImageList.js
+++ src/ImageList.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
+import ImageItem from './ImageItem.js';
 
 export default class ImageList extends Component {
     render() {
         return (
             <div> {
-                this.props.images.map((image) => <ImageItem key={image} image={image}/>)
+                this.props.images.map((image) => <ImageItem key={image.description} image={image}/>)
             }
-            </div>
-        )
-    }
-}
-
-export class ImageItem extends Component {
-    render() {
-        return (
-            <div className="image-container">
-                <p className="image-title">{this.props.image.title}</p>
-                <img src={this.props.image.url} alt=''/>
-                <p>{this.props.image.description}</p>
-                
             </div>
         )
     }
